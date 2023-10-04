68cae8cc8741f847d01a17c2a1a3747fd21b86c9
marcos-wakuluk <marcoswakuluk@Waku-MacBook-Pro.local>
2023-10-04 00:40:05 -0300

    component to render the table, pagination, page selection, creation of reducers to delete and edit

--- src/redux/todoSlice.js
+++ src/redux/todoSlice.js
@@ -11,10 +11,22 @@ export const todoSlice = createSlice({
     addTodo: (state, action) => {
       const todo = action.payload;
       state.value = todo
-    }
+    },
+    editTodo: (state, action) => {
+      const { id, completed } = action.payload;
+      const title = 'fue modificado';
+      const todoIndex = state.value.findIndex(todo => todo.id === id);
+      if (todoIndex !== -1) {
+        state.value[todoIndex].title = title;
+      }
+    },
+    deleteTodo: (state, action) => {
+      const id = action.payload;
+      state.value = state.value.filter(todo => todo.id !== id);
+    },
   }
 })
 
-export const { addTodo } = todoSlice.actions;
+export const { addTodo, editTodo, deleteTodo } = todoSlice.actions;
 
 export default todoSlice.reducer;
