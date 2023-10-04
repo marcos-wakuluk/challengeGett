2f7acfb6d76ff6f0ade59acfd3aad108e799692a
marcos-wakuluk <marcoswakuluk@Waku-MacBook-Pro.local>
2023-10-03 22:16:28 -0300

    add styles

--- src/components/Login.js
+++ src/components/Login.js
@@ -11,37 +11,67 @@ const Login = () => {
 
   const handleLogin = () => {
     if (user === 'user' && password === '1234') {
-      fetch('https://jsonplaceholder.typicode.com/todos')
-        .then(response => response.json())
-        .then(data => dispatch(addTodo(data)))
-        .catch(error => console.log(error))
       setIsLoggedIn(true)
     } else {
-      alert('Incorrect credentials')
+      alert('El nombre de usuario y/o la contraseña son incorrectas')
     }
   }
 
   if (isLoggedIn) {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+      .then((response) => response.json())
+      .then((data) => dispatch(addTodo(data)))
+      .catch((error) => console.log(error))
+
     return <Navigate to="/home" />
   }
 
   return (
-    <div>
-      <input
-        type="text"
-        placeholder="User"
-        value={user}
-        onChange={(event) => setUser(event.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(event) => setPassword(event.target.value)}
-      />
-      <button onClick={handleLogin}>Login</button>
+    <div className="container mt-auto">
+      <div className="row justify-content-center">
+        <div>
+          <div className="card shadow">
+            <div className="card-body">
+              <h2 className="card-title text-center">Welcome!</h2>
+              <form>
+                <div className="form-floating m-2">
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="user"
+                    placeholder="User"
+                    value={user}
+                    onChange={(event) => setUser(event.target.value)}
+                  />
+                  <label htmlFor="user">User</label>
+                </div>
+                <div className="form-floating m-2">
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(event) => setPassword(event.target.value)}
+                  />
+                  <label htmlFor="password">Password</label>
+                </div>
+                <div className="text-center">
+                  <button
+                    className="btn btn-primary"
+                    onClick={handleLogin}
+                  >
+                    Login
+                  </button>
+                </div>
+              </form>
+
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
-}
+};
 
 export default Login;
